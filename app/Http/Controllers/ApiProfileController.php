b3534e930a3b38e1d90a48abaaa13c22fae4796f
monsteronfire <monsteronfire@gmail.com>
2015-07-30 16:10:42 +0800

    adds changed files

--- app/Http/Controllers/ApiProfileController.php
+++ app/Http/Controllers/ApiProfileController.php
@@ -47,96 +47,7 @@ Class ApiProfileController extends Controller {
 		
 		$userId = Request::input('user_id');
 		$profileInfo = [];
-<<<<<<< HEAD
 		try {
-		 	$profiles = GameProfile::select('id','user_id', 'class_id', 'first_name' , 'last_name','age','school','grade' ,'city','email','nickname1','nickname2','avatar_id')->where('user_id', $userId)->orderBy('id')->get();
-
-			foreach($profiles as $profile){
-				$profile->nickName1;
-				$profile->nickName2;
-				$profile->avatar;
-				$profile->gameCode;
-			}
-
-			$sql = "
-				SELECT  *
-					FROM  (
-				        	SELECT p.`id` , gp.`created_at`
-				        		FROM (`t0111_game_profile` p)
-				        			LEFT JOIN `t0400_game_play` gp ON (gp.`profile_id` = p.`id`)
-				        		WHERE p.`user_id` = {$userId}
-				        		AND p.`deleted_at` IS NULL
-				        		ORDER BY gp.`created_at` DESC
-
-				            )t
-				           GROUP BY `id`
-			";
-
-			$lastPlayed = DB::select($sql);
-			for($i=0; $i<count($profiles); $i++){
-		 		$p = $profiles[$i];
-		 		$lp = $lastPlayed[$i];
-
-		 		array_push($profileInfo, [
-		 				'id' => $p->id,
-			 			'user_id' => $p->user_id,
-			 			'class_id' => $p->class_id,
-			 			'first_name' => $p->first_name,
-			 			'last_name' => $p->last_name,
-			 			'age' => $p->age,
-			 			'school' => $p->school,
-			 			'grade' => $p->grade,
-			 			'city' => $p->city,
-			 			'email' => $p->email,
-			 			'nickname1' => $p->nickname1,
-			 			'nickname2' => $p->nickname2,
-			 			'avatar_id' => $p->avatar_id,
-			 			'nick_name1' => $p->nickName1,
-			 			'nick_name2' => $p->nickName2,
-			 			'avatar' => $p->avatar,
-			 			'game_code' => $p->gameCode,
-			 			'best_score' => [],
-			 			'weak_score' => [],
-			 			'last_played' => $lp->created_at,
-			 			
-		 			]);
-		 		$best_score = ApiProfileHelper::ProfileBestScore($p, $scoreType = 'best_score');
-				$weak_score = ApiProfileHelper::ProfileBestScore($p, $scoreType = 'weak_score');
-				 	for($j=0; $j<count($best_score); $j++){
-				 		$b = $best_score[$j];
-				 		array_push($profileInfo[$i]['best_score'], [
-				 			'system_name' => $b->name,
-				 			'planet_id' => $b->planet_id,
-				 			'description' => $b->description,
-				 			'best_score'=> $b->score,
-				 			'status'=>$b->status,
-				 			'play_id'=>$b->play_id,
-				 			]);
-				 	}
-				 	for($k=0; $k<count($weak_score); $k++){
-				 		$w = $weak_score[$k];
-				 		array_push($profileInfo[$i]['weak_score'], [
-				 			'system_name' => $w->name,
-				 			'planet_id' => $w->planet_id,
-				 			'description' => $w->description,
-				 			'weak_score'=> $w->score,
-				 			'status'=>$w->status,
-				 			'play_id'=>$w->play_id,
-				 			]);
-				 	}
-			}
-
-			return ResponseHelper::OutputJSON('success', '', ['list' => $profileInfo]);
-
-		} catch (Exception $ex) {
-			LogHelper::LogToDatabase($ex->getMessage(), ['environment' => json_encode([
-				'source' => 'ApiProfileController > get',
-				'inputs' => Request::all(),
-			])]);
-			return ResponseHelper::OutputJSON('exception');
-		}	
-=======
-		// try {
 	 	$profiles = GameProfile::select('id','user_id', 'class_id', 'first_name' , 'last_name','age','school','grade' ,'city','email','nickname1','nickname2','avatar_id')->where('user_id', $userId)->get();
 
 		foreach($profiles as $profile){
@@ -198,14 +109,13 @@ Class ApiProfileController extends Controller {
 
 			return ResponseHelper::OutputJSON('success', '', ['list' => $profileInfo]);
 
-			// } catch (Exception $ex) {
-			// 	LogHelper::LogToDatabase($ex->getMessage(), ['environment' => json_encode([
-			// 		'source' => 'ApiProfileController > get',
-			// 		'inputs' => Request::all(),
-			// 	])]);
-			// 	return ResponseHelper::OutputJSON('exception');
-			// }	
->>>>>>> adds for each for age and grade in profiles blade
+			} catch (Exception $ex) {
+				LogHelper::LogToDatabase($ex->getMessage(), ['environment' => json_encode([
+					'source' => 'ApiProfileController > get',
+					'inputs' => Request::all(),
+				])]);
+				return ResponseHelper::OutputJSON('exception');
+			}	
 	}
 
 	public function create() {
@@ -423,7 +333,7 @@ Class ApiProfileController extends Controller {
 				return ResponseHelper::OutputJSON('fail','wrong user id');
 			}	
 			
-			if($count == 1){
+			if($count = 1){
 				return ResponseHelper::OutputJSON('fail','at least once profile in account');
 			}
 
