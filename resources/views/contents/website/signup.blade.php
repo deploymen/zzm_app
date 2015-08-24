68b74f1ef88e4205b364dd3608a1ce78a47a7b52
monsteronfire <monsteronfire@gmail.com>
2015-08-24 15:04:14 +0800

    creates activation success and fail pages redirects to signin after signup reverts to jquery from angular because of time constraints

--- resources/views/contents/website/signup.blade.php
+++ resources/views/contents/website/signup.blade.php
@@ -35,125 +35,81 @@
         <link rel="icon" href="favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="/css/main/app.css">
     </head>
-    <body ng-app="signupApp" ng-controller="SignupController">
+    <body>
         <!--[if IE 8]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
-        <div class="site-wrapper sign-in-up">
-            <div class="row">
-                <section id="signup-holder" class="signup-holder cf">
+    
+    <div class="site-wrapper sign-in-up">
+      <div class="row">
+        <section id="signup-holder" class="signup-holder cf">
                     <section class="signup-holder-inner small-12 medium-4 medium-centered columns">
                         <div class="logo">
                             <img src="/assets/main/img/global/logo-main-white.png" alt=" ">
                         </div>
-                        <form name="signup_form" id="signup-form" class="signup-form" novalidate ng-submit="signupForm()">
+                        <form data-abide="ajax" id="signup-form" class="signup-form" novalidate="novalidate">
                             <div class="row">
-                                <div class="small-6 columns">
-                                    <label for="radio-parent" class="">
-                                        <input name="role" type="radio" id="radio-parent" ng-model="role" required="!role" value="parent" ng-focus> I am a parent
-                                    </label>
-                                </div>
+                  <div class="small-6 columns">
+                    <label for="radio-parent" class="">
+                      <input name="role" type="radio" id="radio-parent" required="" value="parent"> I am a parent
+                    </label>
+                  </div>
 
-                                <div class="small-6 columns">
-                                    <label for="radio-teacher" class="">
-                                        <input name="role" type="radio" id="radio-teacher" ng-model="role" required="!role" value="teacher"> I am a teacher
-                                    </label>
-                                </div>
-                                <div class="error-container" 
-                                     ng-show="signup_form.role.$dirty && signup_form.role.$invalid && signup_form.submitted">
-                                  <small class="error" 
-                                         ng-show="signup_form.role.$error.required">
-                                         Please specify a role.
-                                  </small>
-                                </div>
-                            </div>
+                  <div class="small-6 columns">
+                    <label for="radio-teacher" class="">
+                      <input name="role" type="radio" id="radio-teacher" required="" value="teacher"> I am a teacher
+                    </label>
+                  </div>
+                  <small class="error">Please select a role</small>
+                </div>
 
-                            <div class="row">
-                              <div class="small-12 columns">
-                                <label for="users-name" class="label-header">
+                  <div class="row">
+                    <div class="small-12 columns">
+                      <label for="users_name" class="label-header">
                                 Name
-                                  <input type="text" id="users-name" placeholder="John Jones" name="name" required ng-model="name" ng-minlength=3>
-                                </label>
-                                <div class="error-container" 
-                                    ng-show="signup_form.uname.$dirty && signup_form.uname.$invalid && signup_form.submitted">
-                                <small class="error" 
-                                    ng-show="signup_form.uname.$error.required">
-                                    Your name is required.
-                                </small>
-                                <small class="error" 
-                                        ng-show="signup_form.uname.$error.minlength">
-                                        Your name is required to be at least 3 characters
-                                </small>
-                              </div>
-                              </div>
-                            </div>
+                        <input type="text" id="users_name" placeholder="John Jones" name="users_name" required pattern="[a-zA-Z]+">
+                      </label>
+                      <small class="error">Please fill in your name, we like to address our friends properly!</small>
+                    </div>
+                  </div>
 
-                            <div class="row">
-                              <div class="small-12 columns">
-                                <label for="users_email" class="label-header">
+                  <div class="row">
+                    <div class="small-12 columns">
+                      <label for="email" class="label-header">
                                     Email
-                                  <input type="email" id="users_email" placeholder="johnjones@internet.com" name="email" required ng-model="email">
-                                </label>
-                                <div class="error-container" 
-                                     ng-show="signup_form.email.$dirty && signup_form.email.$invalid && signup_form.submitted">
-                                  <small class="error" 
-                                         ng-show="signup_form.email.$error.required">
-                                         Your email is required.
-                                  </small>
-                                  <small class="error" 
-                                         ng-show="signup_form.email.$error.email">
-                                         That is not a valid email. Please input a valid email.
-                                  </small>
-                                </div>
-                              </div>
-                            </div>
+                        <input type="email" id="users_email" placeholder="johnjones@internet.com" name="email" required>
+                      </label>
+                      <small class="error">Please input a valid email address. Like example@domain.com</small>
+                    </div>
+                  </div>
 
-                            <div class="row">
-                              <div class="small-12 columns">
-                                <label for="users_password" class="label-header">
+                  <div class="row">
+                    <div class="small-12 columns">
+                      <label for="password" class="label-header">
                                 Password
-                                  <input type="password" id="users_password" placeholder="......" name="users_password" ng-model="password" required ng-minlength=6>
-                                </label>
-                                <div class="error-container" 
-                                     ng-show="signup_form.users_password.$dirty && signup_form.users_password.$invalid && signup_form.submitted">
-                                  <small class="error" 
-                                         ng-show="signup_form.users_password.$error.required">
-                                         You need to have a password.
-                                  </small>
-                                  <small class="error" 
-                                         ng-show="signup_form.users_password.$error.minlength">
-                                         Your password needs to be at least 6 characters long.
-                                  </small>
-                                </div>
-                              </div>
-                            </div>
+                        <input type="password" id="users_password" placeholder="......" name="password" required>
+                      </label>
+                      <small class="error">Passwords must be at least 6 characters with 1 capital letter, 1 number, and one special character.</small>
+                    </div>
+                  </div>
 
-                            <div class="row">
-                              <div class="small-12 columns">
-                                <label for="users_country" class="label-header">
-                                Country
-                                  <select id="users_country" name="country" required ng-model="country" ng-options="country.name for country in countries track by country.code">
-                                    <option value="">-- Select a Country --</option>
-                                  </select>
-                                </label>
-                                <div class="error-container" 
-                                     ng-show="signup_form.country.$dirty && signup_form.country.$invalid && signup_form.submitted">
-                                  <small class="error" 
-                                         ng-show="signup_form.country.$error.required">
-                                         Please choose your country from the list.
-                                  </small>
-                                </div>
-                              </div>
-                            </div>
+                  <div class="row" ng-app="countrydropdown" ng-controller="countryController">
+                    <div class="small-12 columns">
+                      <label for="country" class="label-header">
+                          Country
+                            <select id="users_country" name="country" required ng-model="country" ng-options="country.name for country in countries track by country.code">
+                              <option value="">-- Select a Country --</option>
+                            </select>
+                      </label>
+                      <small class="error">Please specify a country</small>
+                    </div>
+                  </div>
 
-                            <div class="row">
-                              <div class="small-12 columns">
-                                <button data-ng-disabled="progress.active()" data-ng-click="submit(signup_form)" class="medium radius button expand blue">Submit</button>
-                                <!-- <button type="submit" ng-disabled="signup_form.$invalid" class="button radius blue">Submit</button> -->
-                                <!-- <button id="btn-signup" type="submit" ng-disabled="signup_form.$invalid" class="medium radius button expand blue">Sign me up!</button> -->
-                              </div>
-                            </div>
+                  <div class="row">
+                    <div class="small-12 columns">
+                      <button id="btn-signup" type="submit" class="medium radius button expand blue">Sign me up!</button>
+                    </div>
+                  </div>
                         </form>
                         <hr>
                         <section class="prompt-login-wrapper">
@@ -161,9 +117,9 @@
                         </section>
                     </section><!--signup-holder-inner-->
                 </section>
-            </div>
-            <div class="blue-bg-overlay"></div>
-        </div><!--site-wrapper-->
+      </div>
+      <div class="blue-bg-overlay"></div>
+    </div><!--site-wrapper-->
 
         <!-- SCRIPTS START HERE -->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -171,7 +127,6 @@
 
         <script src="/js/main/app.js"></script>
         <script src="/js/main/init.js"></script>
-        <script src="/js/main/angular-promise-tracker/promise-tracker.js"></script>
         <script src="/js/main/signup.js"></script>
 
         <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
