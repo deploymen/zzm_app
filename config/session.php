e113b3598a7f9a8d6c1fec16ab13440128be3def
Lai Wei Zhong <weizhong.lai@webuiltit.com.my>
2016-03-04 16:44:33 +0800

    session drive

--- config/session.php
+++ config/session.php
@@ -16,7 +16,7 @@ return [
 	|
 	*/
 
-	'driver' => env('SESSION_DRIVER', 'file'),
+	'driver' => env('SESSION_DRIVER', 'cookie'),
 
 	/*
 	|--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
 	|
 	*/
 
-	'lifetime' => 1051200,
+	'lifetime' => 10080,//105120
 
 	'expire_on_close' => false,
 
