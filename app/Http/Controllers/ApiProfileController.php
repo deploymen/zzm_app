5965433a394f1cadda04dc67c6416f26d2114fdd
Staging Server <dev@zapzapmath.com>
2016-03-15 09:20:01 +0000

    > Check

--- app/Http/Controllers/ApiProfileController.php
+++ app/Http/Controllers/ApiProfileController.php
@@ -602,4 +602,40 @@ Class ApiProfileController extends Controller {
 			return ResponseHelper::OutputJSON('exception');
 		}
 	}
+
+
+	public function unlockUserLimit() {
+	
+		$fb = new Facebook([
+            'app_id' => env('FACEBOOK_APP_KEY'),
+            'app_secret' => env('FACEBOOK_APP_SECRET'),
+            'default_graph_version' => 'v2.5',
+       	 ]);
+​
+        $helper = $fb->getJavaScriptHelper();
+​
+        try {
+            $accessToken = $helper->getAccessToken();
+        } catch (Facebook\Exceptions\FacebookResponseException $e) {
+            // When Graph returns an error
+            echo 'Graph returned an error: ' . $e->getMessage();
+            exit;
+        } catch (Facebook\Exceptions\FacebookSDKException $e) {
+            // When validation fails or other local issues
+            echo 'Facebook SDK returned an error: ' . $e->getMessage();
+            exit;
+        }
+​
+        if (!isset($accessToken)) {
+            echo 'No cookie set or no OAuth data could be obtained from cookie.';
+            exit;
+        }
+​
+        $postId = Request::input('post_id');
+        $response = $fb->get('/' . $postId, $accessToken->getValue());
+        $graphObject = $response->getGraphObject();
+​
+        var_export($graphObject);
+        die();
+	}
 }
