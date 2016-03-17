c9dccd82d2091acc1d9be28d66d672deb3c370db
Staging Server <dev@zapzapmath.com>
2016-03-17 10:40:02 +0000

    > Check

--- app/Libraries/ZapZapQuestionHelper.php
+++ app/Libraries/ZapZapQuestionHelper.php
@@ -331,8 +331,9 @@ class ZapZapQuestionHelper{
 			}
 
 			$sql = "
-				SELECT p03.*, qc.`question_id`
-					FROM `t0203_game_question_p03` p03, `t0126_game_planet_question_cache` qc
+				SELECT p03.*, qc.`question_id` , obj.`question_object_1`,obj.`question_object_2`
+					FROM (`t0203_game_question_p03` p03, `t0126_game_planet_question_cache` qc)
+						LEFT JOIN `t0203_game_question_p03_object` obj ON (obj.`question_id` = p03.`id` )
                         WHERE qc.`planet_id` = {$planetId}
                         	AND qc.`difficulty` = {$difficulty}
                         	AND p03.`id` = qc.`target_id`
@@ -353,6 +354,8 @@ class ZapZapQuestionHelper{
 					array_push($results, [
 						'id' => $r->question_id,
 						'question' => $r->question,
+						'question_object_1' => $r->question_object_1,
+						'question_object_2' => $r->question_object_2,
 						'answer' => $r->answer,
 						'answer_option_1' => $r->answer_option_1,
 						'answer_option_2' => $r->answer_option_2,
