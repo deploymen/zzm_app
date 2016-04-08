14c07c98bb0afd2bf7e70aacea508e61def16011
Lai Wei Zhong <weizhong.lai@webuiltit.com.my>
2016-04-08 10:42:51 +0800

    route 1.2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,8 +19,12 @@ Route::group(['middleware' => 'version'], function () {
 		require __DIR__.'/Routes/1.1/api.php';
 		require __DIR__.'/Routes/1.1/api-user.php';
 		require __DIR__.'/Routes/1.1/api-game.php';
-
-		
-
 	});
+
+	Route::group(['prefix' => '/1.2'], function () {
+		require __DIR__.'/Routes/1.2/api.php';
+		require __DIR__.'/Routes/1.2/api-user.php';
+		require __DIR__.'/Routes/1.2/api-game.php';
+	});
+
 });
