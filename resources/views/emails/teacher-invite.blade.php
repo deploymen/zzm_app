6e9a3f15af9214a60b895ea9e64ff8ccd9319f64
Lai Wei Zhong <weizhong.lai@webuiltit.com.my>
2016-04-14 18:32:20 +0800

    invite teacher

---
+++ resources/views/emails/teacher-invite.blade.php
@@ -0,0 +1,16 @@
+<html>
+Hi <name>,<br>
+<br>
+Greetings from Zapien! We are from Zap Zap Math, math platform that makes learning math fun and interactive for Kindergarten to Primary 6. <br>
+<br>
+Your colleague, <name> needs your help to activate giveaways for your school. Sign up to Zap Zap Math to help your school get a 2 years free subscription for all students in your school. (You need to sign up from your school email).<br>
+<br>
+However, if you have any questions about the journey, feel free to get in touch with us. (hello@zapzapmath.com)<br>
+<br>
+We look forward to adventuring with you!<br>
+<br>
+Zap Zap Math Team<br>
+{{$zzm_url}}<br>
+{{$social_media_links}}
+</html>
+
