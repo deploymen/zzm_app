fe7a04e530cb55950d90b4d30f3b18d794ba1c2c
Lai Wei Zhong <weizhong.lai@webuiltit.com.my>
2016-08-18 19:05:05 +0800

    route update

--- app/Http/Routes/1.3/api-game.php
+++ app/Http/Routes/1.3/api-game.php
@@ -45,7 +45,7 @@ Route::group(['prefix' => 'game'], function () {
 		Route::get('/result/system-planet/progress', 'ApiGameController@systemPlanetProgress');
 		Route::get('/result/system-planet/play/planet/{planet_id}', 'ApiGameController@systemPlanetPlay');
 
-		Route::get('/user-map', 'ApiGameController@getUserMapV1_2');
+		Route::get('/user-map', 'ApiGameController@getUserMapV1_3');
 
 		Route::get('/result/only-system', 'ResultController@onlySystemV1_1');
 		Route::get('/result/only-planet', 'ResultController@onlyPlanetV1_1');
