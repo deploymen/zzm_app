81569b9a784ab816f6b1164234fad5fdb1d7c363
Mark Leong <markyleong@gmail.com>
2016-10-31 12:59:56 +0000

    Remove unneeded view files

--- resources/views/shared/user_topbar.blade.php
+++
@@ -1,6 +0,0 @@
-<header class="user_topbar">
-    <a class="left-off-canvas-toggle" href="#"><i class="fa fa-bars"></i></a>
-    <img src="{{ config('zzm.cdn_static') }}/images/global/logo-z.png" />
-    <h1>Zap Zap Math</h1>
-    <span class="type">Basic</span>
-</header>
