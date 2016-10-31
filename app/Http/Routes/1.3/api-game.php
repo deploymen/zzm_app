76712230da47b0516434faf1f7ae7917c32a8ba7
Mark Leong <markyleong@gmail.com>
2016-10-31 11:59:09 +0000

    Auto format Http/Routes

--- app/Http/Routes/1.3/api-game.php
+++ app/Http/Routes/1.3/api-game.php
@@ -1,42 +1,45 @@
 <?php
+
 // =======================================================================//
 // ! Game Profile(Kid)										 			      //
 // =======================================================================//
 Route::group(['prefix' => 'game'], function () {
-	Route::get('/create-package', 'ApiGameController@createPackage');
+    Route::get('/create-package', 'ApiGameController@createPackage');
 
-	Route::post('/sign-up', 'AuthUserController@signUpApp');
-	Route::post('student-id/anonymous', 'ApiProfileController@GenerateAnonymousStudentId');
+    Route::post('/sign-up', 'AuthUserController@signUpApp');
+    Route::post('student-id/anonymous', 'ApiProfileController@GenerateAnonymousStudentId');
 
 
-	Route::group(['middleware' => ['auth.student' , 'auth.cmd']], function () {
-		Route::post('/check-student-id' , 'ApiGameController@checkStudentIdV1_3');
-		Route::post('/student-id', 'ApiGameController@getStudentIdInfo');
+    Route::group(['middleware' => ['auth.student', 'auth.cmd']], function () {
+        Route::post('/check-student-id', 'ApiGameController@checkStudentIdV1_3');
+        Route::post('/student-id', 'ApiGameController@getStudentIdInfo');
 
-		Route::put('/profiles', 'ApiProfileController@gameUpdateV1_3');
+        Route::put('/profiles', 'ApiProfileController@gameUpdateV1_3');
 
-		Route::post('/verify-transfer', 'ApiProfileController@verifyCodeV1_3');
-		Route::post('/profile-transfer', 'ApiProfileController@profileTransferV1_3');
-		Route::post('/profile-transfer-loose', 'ApiProfileController@profileTransferLooseV1_3');
+        Route::post('/verify-transfer', 'ApiProfileController@verifyCodeV1_3');
+        Route::post('/profile-transfer', 'ApiProfileController@profileTransferV1_3');
+        Route::post('/profile-transfer-loose', 'ApiProfileController@profileTransferLooseV1_3');
 
-		Route::post('/play/{id}/result', 'ApiGameController@resultV1_3');
-		Route::get('/play/{id}/request'  , 'ApiGameController@requestV1_3');
+        Route::post('/play/{id}/result', 'ApiGameController@resultV1_3');
+        Route::get('/play/{id}/request', 'ApiGameController@requestV1_3');
 
-		Route::post('/play/push-result'  , 'ApiGameController@offlinePostV1_3');
+        Route::post('/play/push-result', 'ApiGameController@offlinePostV1_3');
 
-		Route::get('/play/{id}/request/{language}', 'ApiGameController@request');
+        Route::get('/play/{id}/request/{language}', 'ApiGameController@request');
 
-		Route::get('/user-map', 'ApiGameController@getUserMapV1_3');
+        Route::get('/user-map', 'ApiGameController@getUserMapV1_3');
 
-		Route::get('/spaceship', 'ApiGameController@getSpaceship');
-		Route::post('/spaceship/unlock/spaceship', 'ApiGameController@unlockSpaceship');
-		Route::post('/spaceship/unlock/floor', 'ApiGameController@unlockFloor');
-		Route::post('/spaceship/unlock/item', 'ApiGameController@unlockItem');
-		Route::put('/spaceship/floor/{floor_id}/items', 'ApiGameController@spaceshipItemSelected');
+        Route::get('/spaceship', 'ApiGameController@getSpaceship');
+        Route::post('/spaceship/unlock/spaceship', 'ApiGameController@unlockSpaceship');
+        Route::post('/spaceship/unlock/floor', 'ApiGameController@unlockFloor');
+        Route::post('/spaceship/unlock/item', 'ApiGameController@unlockItem');
+        Route::put('/spaceship/floor/{floor_id}/items', 'ApiGameController@spaceshipItemSelected');
 
-		Route::put('add/coin' , 'ApiGameController@addCoin');
-	});
+        Route::put('add/coin', 'ApiGameController@addCoin');
+    });
 });
 
 
-Route::any('/{endpoint}', ['as' => 'try_prev_version', function(){die('NEED TO HANDLE1.3');}])->where('endpoint', '.*');
+Route::any('/{endpoint}', ['as' => 'try_prev_version', function() {
+        die('NEED TO HANDLE1.3');
+    }])->where('endpoint', '.*');
